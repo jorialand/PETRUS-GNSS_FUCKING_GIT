ca096dd9e22decfb0bc71298ef8ab2d2a0ac55d0
horiaghionoiu <horia.ghionoiu@gmail.com>
2021-08-27 18:15:25 +0100

    [T2.4 GAPS][PETRUS-PPVE-REQ-090] Implemented

--- WP1/SRC/COMMON/Utils.py
+++ WP1/SRC/COMMON/Utils.py
@@ -1,3 +1,13 @@
+TESTING = True
+'''
+Be smart, don't be a retard!
+
+# Ver satélite 30
+gawk '$5 == 30' OBS_TLSZ_Y19D014.dat
+
+# Ver satélite 30 e imprimir líneas 1 a 10
+gawk  '$5 == 30' OBS_TLSZ_Y19D014.dat | sed -n '1,10p;11q'
+'''
 def get_sat_label(PRN):
     return "G" + "%02d" % int(PRN)
 
